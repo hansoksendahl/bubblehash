df87d3355b091554d71996d0b1e0387d016dd528
Hans Oksendahl <hansoksendahl@gmail.com>
2014-02-18 20:17:43 -0500

    testing

--- assets/scripts/handler.js
+++ assets/scripts/handler.js
@@ -42,7 +42,6 @@ pcOffer = bubblehash.rtc(commServer, commOptions);
 // Initialize a WebRTC offer.
 pcOffer.call(function (description) {
   offer = description;
-  shareOffer();
 });
 
 function shareOffer (callback) {
