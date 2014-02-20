3f85a20906bcbb2af265141025812f29a5d941e6
Hans Oksendahl <hansoksendahl@gmail.com>
2014-02-19 20:14:03 -0500

    Seeing if I can get a connection established

--- assets/scripts/handler.js
+++ assets/scripts/handler.js
@@ -9,19 +9,31 @@ if (
 }
 
 // Initialize variables used in the app
-var iFace
+var iFace = {}
   , commServer
   , commOptions
   , commSilo
-  , pcOffer
-  , offer
-  , answers
-  , offer;
-  
-// Create refercenes to all needed app interafce items.
-iFace.fldOffer = $("#fldOfferURL");
-iFace.btnHost = $("#btnHost");
-iFace.modLocalOffer = $("#modShowLocalOffer")
+  , pc;
+
+// Create jQuery selectors for each of the following ids
+[
+  "fldLocalOffer",
+  "fldRemoteOffer",
+  "fldRemoteAnswer",
+  "fldLocalAnswer",
+  "modLocalOffer",
+  "modLocalAnswer",
+  "modRemoteOffer",
+  "modRemoteAnswer",
+  "btnInvite",
+  "btnJoin",
+  "btnCreateOffer",
+  "btnAcceptAnswer",
+  "btnCreateAnswer",
+  "btnAcceptOffer"
+].forEach(function (e) {
+  iFace[e] = $("#"+e);
+});
 
 // Specify the communications Silo
 commSilo = "http://mudb.org";
@@ -41,32 +53,78 @@ commOptions = {
   ]
 };
 
-// Create a new PeerConnection object
-pcOffer = bubblehash.rtc(commServer, commOptions);
-
-// Initialize a WebRTC offer.
-pcOffer.call(function (description) {
-  offer = description;
-});
-
-function shareOffer (callback) {
-  bubblehash.xhr(commSilo+"/set/json")
-    .data(offer)
-    .post(function () {
-      var data = (JSON.parse(this.responseText));
-      iFace.fldOffer.val(data.url);
-      iFace.modLocalOffer.modal();
-      iFace.fldOffer.focus();
-    });
+// Initialize a PeerConnection
+function initPeer () {
+  pc = bubblehash.rtc(commServer, commOptions);
 }
 
-function select () {
-  this.select();
+// Create an Invite URL and show the local offer modal window
+function setOffer () {
+  initPeer();
+  
+  // Initialize a WebRTC offer.
+  pc.call(function (description) {
+    bubblehash.xhr(commSilo+"/set/json")
+      .data(description)
+      .post(function () {
+        var data = JSON.parse(this.responseText);
+        iFace.fldLocalOffer.val(data.url);
+        iFace.modLocalOffer.modal();
+        iFace.fldLocalOffer.focus();
+        iFace.btnCreateOffer.click(getAnswer);
+      });
+  });
 }
+
+// Get the answer sent from a peer
+function getAnswer () {
+  iFace.modRemoteAnswer.modal();
+  iFace.fldRemoteAnswer.focus();
+  iFace.btnAcceptAnswer.click(acceptAnswer);
+}
+
+// Establish the connection
+function acceptAnswer () {
+  var pcAnswer = bubblehash.rtc(commServer, commOptions);
+  
+}
+
+// Get the offer sent from a host
+function getOffer () {
+  iFace.modRemoteOffer.modal();
+  iFace.fldRemoteOffer.focus();
+  iFace.btnCreateAnswer.click(setAnswer);
+}
+
+// Create an RSVP URL and show the local answer modal window
+function setAnswer () {
+  // Get the offer associated with the url.
+  bubblehash.xhr(iFace.fldRemoteOffer.val()).get(function () {
+    var data = JSON.parse(this.responseText);
+    
+    initPeer();
+    
+    pc.answer(new RTCSessionDescription(data.sdp), function (description) {
+      bubblehash.xhr(commSilo+"/set/json")
+        .data(description)
+        .post(function () {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+          });
+        });
+  });
+}
+
+// Check that the connection has been established
+function acceptOffer () {
+  
+}
+
 
 // Make text selected on focus
-iFace.fldOffer
-    .focus(select)
-    .mouseenter(select)
+iFace.fldLocalOffer
+    .focus(function () { this.select(); })
+    .mouseup(function (){ return false; })
 
-iFace.btnHost.click(shareOffer);
+iFace.btnInvite.click(setOffer);
+iFace.btnJoin.click(getOffer);
