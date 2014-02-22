2cbcbaeef40feb8c70ed3ecdb56aa792d480855d
Hans Oksendahl <hansoksendahl@gmail.com>
2014-02-21 20:10:31 -0500

    test

--- assets/scripts/bubblehash.js
+++ assets/scripts/bubblehash.js
@@ -246,7 +246,7 @@ var rtc = (function rtc() {
             }
             out.addIceCandidate = function(candidate, success) {
                 // Success and failure functions are not currently supported in chrome
-                success = log.warn(0x0005, success)();
+                success = log.message(0x0005, success)();
                 connection.addIceCandidate(new iceCandidate(candidate));
             }
 
