70581bdbf023922aeacd7b977cda61dbd79e67ec
Hans Oksendahl <hansoksendahl@gmail.com>
2014-02-26 15:57:22 -0500

    test

--- assets/scripts/bubblehash.js
+++ assets/scripts/bubblehash.js
@@ -679,9 +679,7 @@ var BubbleHash = (function() {
         ui.search.removeAttr("disabled");
         ui.searchQuery.removeAttr("disabled");
 
-        if (bubblehash.connections.length === 1) {
-            ui.searchQuery.tooltip("show");
-        }
+        ui.searchQuery.tooltip("show");
 
         // Hide the form fields and update status if connections list is empty.
         dataConnection.once("close", connectClose);
--- src/interface/ui/peer/connection.js
+++ src/interface/ui/peer/connection.js
@@ -4,9 +4,7 @@ function peerConnection (dataConnection) {
   ui.search.removeAttr("disabled");
   ui.searchQuery.removeAttr("disabled");
   
-  if (bubblehash.connections.length === 1) {
-    ui.searchQuery.tooltip("show");
-  }
+  ui.searchQuery.tooltip("show");
   
   // Hide the form fields and update status if connections list is empty.
   dataConnection.once("close", connectClose);
